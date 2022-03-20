376ffc60ee7a00a0f3dbafee277ee47985be71a4
clarisse <gngondiop@yhoo.fr>
2022-03-20 03:16:21 -0400

    terraform code

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+region        = "us-east-1"
+ami           = "ami-0c02fb55956c7d316"
+keypair       = "devops"
+instance-type = "t2.micro"
---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
+variable "ami" {
+    type = string
+    default = "ami-0c02fb55956c7d316"
+}
+variable "instance-type" {
+    type = string
+    default = "t2.micro"
+}
+variable "keypair" {
+    type = string
+    default = "devops"
+}
